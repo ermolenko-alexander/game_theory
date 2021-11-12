a7ed982920e8e0fc8438a9b27846f8dedebee175
Xelanid <ermolenko.alexander@gmail.com>
2021-11-12 20:23:37 +0300

    Добавлена функция составления матрицы по заданным ограничениям и добавленным слабым переменным

--- simplex-method.py
+++ simplex-method.py
@@ -1,7 +1,54 @@
 import numpy as np
+from fractions import Fraction
 
 
-def simplex(n_vars, equations, function):
+def coeff_matrix(n_x_vars, equations, function):
+    """
+    :param n_x_vars: Число переменных в задаче ЛП
+
+    :param equations: Список элементов типа string
+    
+    :param function: Тип list с двумя параметрами:
+                            1. min или max
+                            2. функция, с которой работаем
+    """
+    
+    n_s_vars = 0
+    total_vars = n_x_vars
+    
+    # добавление слабых переменных в ограничения
+    for equation in equations:
+        if '>=' in equation or '<=' in equation:
+            n_s_vars += 1
+            total_vars += 1
+        
+    coeff_matrix = [[Fraction("0/1") for i in range(total_vars+1)] for j in range(len(equations)+1)]        
+    s_index = n_x_vars # индекс, начиная с которого будут располагаться значения s в матрице
+    
+    for i in range(1, len(equations)+1):
+        equation = equations[i - 1].split(' ') # разбиваем ограничение по пробелам, чтобы уметь доставать любую переменную, коэффициент или операцию
+        
+        for j in range(len(equation)):
+            if 'x_' in equation[j]:
+                coeff, index = equation[j].split('x_') # т.к. переменные у нас вида ax_i, то разделяя по x_, вытаскиваем коэффициент и индекс переменной
+                
+                if equation[j - 1] == '-':
+                    coeff_matrix[i][int(index) - 1] = Fraction("-" + coeff + "/1")
+                else:
+                    coeff_matrix[i][int(index) - 1] = Fraction(coeff + "/1")
+            
+            elif equation[j] == '<=':
+                coeff_matrix[i][s_index] = Fraction("1/1")
+                s_index += 1
+                
+            elif equation[j] == '>=':
+                coeff_matrix[i][s_index] = Fraction("-1/1") 
+                s_index += 1
+                
+    return coeff_matrix
+
+
+def simplex(n_x_vars, equations, function):
     """
     :param vars: Число переменных в задаче ЛП
 
@@ -18,7 +65,49 @@ def simplex(n_vars, equations, function):
                             1. min или max
                             2. функция, с которой работаем
     """
-    pass
+    coeff_matrix(n_x_vars, equations, function)
+
+
+def input_file():
+    print("Для корректного ввода прочитайте правила оформления в файле README.md")
+    print("Введите название файла")
+    file = input()
+    
+    f = open(file, "r", encoding='utf-8')
+    equations = []
+    k = 0
+    for line in f:
+        if k == 0:
+            n_vars = line.replace('\n', '')
+        elif k == 1:
+            function = line.replace('\n', '')
+        else:
+            equations.append(line.replace('\n', ''))
+        k += 1
+        
+    print(function, equations, n_vars)
+    
+    return function, equations, n_vars
+    
+
+def input_console():
+    print("Для корректного ввода прочитайте правила оформления в файле README.md")
+    print("Введите функцию и цель исследования - min/max - через запятую и пробел после нее")
+    function = input().split(', ')
+    print(function)
+    
+    print("Введите количество переменных:")
+    n_vars = int(input())
+    
+    print("Введите количество ограничений:")
+    n = int(input())
+    
+    print(f"На каждой строке через enter введите {n} ограничений:")
+    equations = []
+    for i in range(n):
+        equations.append(input())
+        
+    return function, equations, n_vars
 
 
 def input_file():
