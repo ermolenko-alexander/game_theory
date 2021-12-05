62a2d49431318851979c7021df0d8aac0b87aad8
Xelanid <ermolenko.alexander@gmail.com>
2021-12-05 18:48:11 +0300

    Исправлен выбор ведущего столбца

--- Simplex method/iteration.py
+++ Simplex method/iteration.py
@@ -22,28 +22,29 @@ def unlimited_check(coeff_table, lead_column):
     Returns:
         lead_row (int): Номер ведущей строки
     """
-    fl = coeff_table[lead_column][1:].all() <= 0
+    fl = np.array(coeff_table[lead_column][1:].any()) <= 0
     
     if fl:
         print("Нет оптимального решения")
     else:
         a_rs = np.array(coeff_table[lead_column][1:])
         b = np.array(coeff_table['basic'][1:])
-        
+
         res = []
         i = 0
-        min_row = a[0]
+        min_row = 1000
+        lead_row = 0
         for a in a_rs:
             if a > 0:
-                if a / b[i] < min_row:
-                    min_row = a / b[i]
+                if b[i] / a < min_row:
+                    min_row = b[i] / a
                     lead_row = i
             i += 1
 
-        return lead_row
+        return lead_row + 1
 
 
-def optimality_check(coeff_table, columns):
+def optimality_check(coeff_table, n_x_vars, columns):
     """
     Проверка условия оптимальности:
         Если все коэффициенты в нулевой строке симплексной таблицы,
@@ -60,26 +61,29 @@ def optimality_check(coeff_table, columns):
     Args:
         param coeff_table (DataFrame): Симплекс-таблица
         param columns (list): Список названий столбцов
+        param n_x_vars (int): Число исходных переменных x
 
     Returns:
         coeff_table (DataFrame): Итоговая симплекс-таблица
     """
-    fl = (coeff_table.iloc[0]).all() > 0
+    fl = (coeff_table.iloc[0]).any() < 0
     
-    while not fl:
+    while fl:
         row_0 = np.array(coeff_table[coeff_table.columns[:-1]].loc[0])
+        fl = row_0.any() < 0
         
         min_c = row_0.min()
         lead_column = row_0.argmin()
         lead_column = columns[lead_column]
         
-        if fl:
+        if not fl:
             return coeff_table
         else:
             lead_row = unlimited_check(coeff_table, lead_column)
+            lead_column = columns.index(lead_column)
             lead_elem = (lead_row, lead_column)
-            #coeff_table = Gauss(coeff_table, lead_elem, columns)
-            fl = True
+            coeff_table = Gauss(coeff_table, lead_elem, columns)
     
-     # lead_elem = coeff_table[row_index][column_index]
+    # lead_elem = coeff_table[lead_row][lead_column]
+
     return coeff_table
